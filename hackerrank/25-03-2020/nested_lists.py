d4cf5a9b95a279c7322ad82ba86ee3ea79f0ad26
Szymon Matyla <szymon.matyla@gmail.com>
2020-03-25 21:20:15 +0100

    submitted tdd hacks on nested lists

--- hackerrank/25-03-2020/nested_lists.py
+++ hackerrank/25-03-2020/nested_lists.py
@@ -20,32 +20,40 @@
 # order their names alphabetically and print each one on a new line.
 import operator
 
+
+def sorted_by_score_ascending(how_many_scores, name_scores_list):
+    db1 = []
+    [db1.append([x[0], x[1]]) for x in name_scores_list]
+    db1 = sorted(db1, key=lambda x: x[1], reverse=False)
+    return db1
+
+
+def find_second_lowest_ordered_by_name(given_scores_list):
+    db1 = []
+    db1 = sorted_by_score_ascending(len(given_scores_list), given_scores_list)
+    new_db = []
+    [new_db.append(item) for item in db1 if item[1] == db1[1][1]]
+
+    new_db = sorted(new_db, key=lambda x: x[0], reverse=False)
+
+    if len(new_db) == 1:
+        return new_db[0]
+    else:
+        return new_db
+
+
 if __name__ == '__main__':
 
     db = []
     for _ in range(int(input())):
         name = input()
         score = float(input())
-
         db.append([name, score])
 
-    #   1. playing with sorted
-    #
-    #   sorted(iterable, *, key=None, reverse=False)
-    #
-    #   Return a new sorted list from the items in iterable. Has two optional
-    #   arguments which must be specified as keyword arguments.
-    #
-    #   key specifies a function of one argument that is used
-    #   to extract a comparison key from each element in iterable (for example, key=str.lower). The default value is
-    #   None (compare the elements directly).
-    #
-    #   reverse is a boolean value. If set to True, then the list elements are sorted as if each comparison were reversed.
+    # print(db)
 
-    # db = sorted(db, key=operator.itemgetter(1), reverse=True)
+    ordered_seconds_scores = []
+    ordered_seconds_scores = find_second_lowest_ordered_by_name(db)
 
-    second_element = lambda element: element[1]
-
-    db = sorted(db, key=second_element, reverse=True)
-
-    print(db)
+    for score in ordered_seconds_scores:
+        print(score[0])
